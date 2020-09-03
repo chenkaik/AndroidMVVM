7f02e1a6f9e6a80149d5cbc7c862ea813b08f1d0
chenkai <752337765@qq.com>
2020-09-03 16:31:26 +0800

    first commit

---
+++ app/src/main/java/com/android/mvc/util/ActivityCollector.kt
@@ -0,0 +1,30 @@
+package com.android.mvc.util
+
+import android.app.Activity
+
+/**
+ * date: 2020/7/12
+ * desc: 管理activity
+ */
+object ActivityCollector {
+
+    private val activities = ArrayList<Activity>()
+
+    fun addActivity(activity: Activity) {
+        activities.add(activity)
+    }
+
+    fun removeActivity(activity: Activity) {
+        activities.remove(activity)
+    }
+
+    fun finishAll() {
+        for (activity in activities) {
+            if (!activity.isFinishing) {
+                activity.finish()
+            }
+        }
+        activities.clear()
+    }
+
+}
---
+++ app/src/main/java/com/android/mvc/util/String.kt
@@ -0,0 +1,31 @@
+package com.android.mvc.util
+
+/**
+ * date: 2020/7/15
+ * desc: String扩展函数
+ */
+fun String.lettersCount(): Int { // 计算字符串中包含多少个字母
+    var count = 0
+    for (char in this) {
+        if (char.isLetter()) {
+            count++
+        }
+    }
+    return count
+}
+
+//operator fun String.times(n: Int): String {
+//    val builder = StringBuilder()
+//    repeat(n) {
+//        builder.append(this)
+//    }
+//    return builder.toString()
+//}
+
+/**
+ * 重载times函数 可以让字符串和数字相乘
+ * Kotlin中的String类已经提供了一个用于将字符串重复N次的repeat()函数
+ * 精简编写如下
+ */
+operator fun String.times(n: Int) = repeat(n)
+
