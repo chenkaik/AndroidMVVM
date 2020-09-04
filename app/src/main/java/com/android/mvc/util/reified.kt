2461b27c9fac307ff9b789cd45cceb6b4644d70a
chenkai <752337765@qq.com>
2020-09-04 12:25:37 +0800

    添加泛型实化的使用

---
+++ app/src/main/java/com/android/mvc/util/reified.kt
@@ -0,0 +1,19 @@
+package com.android.mvc.util
+
+import android.content.Context
+import android.content.Intent
+
+/**
+ * date: 2020/8/3
+ * desc: 泛型实化 必须inline关键字声明 声明泛型的地方必须加上reified关键字来表示该泛型要进行实化
+ */
+inline fun <reified T> startActivity(context: Context) {
+    val intent = Intent(context, T::class.java)
+    context.startActivity(intent)
+}
+
+inline fun <reified T> startActivity(context: Context, block: Intent.() -> Unit) {
+    val intent = Intent(context, T::class.java)
+    intent.block()
+    context.startActivity(intent)
+}
