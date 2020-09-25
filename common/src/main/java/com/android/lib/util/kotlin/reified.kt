720c9273b45483ce657667c52647735cdfb07be6
chenkai <752337765@qq.com>
2020-09-25 17:52:26 +0800

    修改activity的跳转方式

--- common/src/main/java/com/android/lib/util/kotlin/reified.kt
+++ common/src/main/java/com/android/lib/util/kotlin/reified.kt
@@ -1,19 +1,42 @@
 package com.android.lib.util.kotlin
 
-import android.content.Context
 import android.content.Intent
+import androidx.fragment.app.FragmentActivity
+import com.android.lib.util.ActivityCollector
 
 /**
  * date: 2020/8/3
  * desc: 泛型实化 必须inline关键字声明 声明泛型的地方必须加上reified关键字来表示该泛型要进行实化
  */
-inline fun <reified T> startActivity(context: Context) {
-    val intent = Intent(context, T::class.java)
-    context.startActivity(intent)
+inline fun <reified T> startActivity(activity: FragmentActivity, isPutStack: Boolean) {
+    val intent = Intent(activity, T::class.java)
+    ActivityCollector.startPage(activity, intent, isPutStack)
 }
 
-inline fun <reified T> startActivity(context: Context, block: Intent.() -> Unit) {
-    val intent = Intent(context, T::class.java)
+inline fun <reified T> startActivity(
+    activity: FragmentActivity,
+    isPutStack: Boolean,
+    requestCode: Int
+) {
+    val intent = Intent(activity, T::class.java)
+    ActivityCollector.startPage(activity, intent, isPutStack, requestCode)
+}
+
+inline fun <reified T> startActivity(
+    activity: FragmentActivity, isPutStack: Boolean,
+    block: Intent.() -> Unit
+) {
+    val intent = Intent(activity, T::class.java)
     intent.block()
-    context.startActivity(intent)
+    ActivityCollector.startPage(activity, intent, isPutStack)
+}
+
+inline fun <reified T> startActivity(
+    activity: FragmentActivity,
+    isPutStack: Boolean, requestCode: Int,
+    block: Intent.() -> Unit
+) {
+    val intent = Intent(activity, T::class.java)
+    intent.block()
+    ActivityCollector.startPage(activity, intent, isPutStack, requestCode)
 }
